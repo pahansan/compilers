ad8e19bb16233c45be348e2e743ca7753f1e246e
Kulik Pavel <pashakulik58@gmail.com>
2025-02-10 00:45:22 +0700

    Половина литералов сделана

---
+++ 1lab/decliterals.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR="folly"
+
+REGEXP="[0]|[1-9][0-9]*(['][0-9]+)*[Zz]?[UuLl]?[UuLl]?[UuLl]?[Zz]?"
+
+for f in $(find $DIR -name "*.cpp"); do
+    echo "*** File $f"
+    sed -e ':a' -e '/\/\*/ { :b; /\*\//!{N; bb}; s|/\*.*\*/|| }' -e 's|//.*||' "$f" |
+    grep -v '^#' |
+    grep -Eo "$REGEXP"
+done
---
+++ 1lab/octliterals.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIR="folly"
+
+REGEXP="0([']?[0-7]+)+[Zz]?[UuLl]?[UuLl]?[UuLl]?[Zz]?"
+
+for f in $(find $DIR -name "*.cpp"); do
+    echo "*** File $f"
+    sed -e ':a' -e '/\/\*/ { :b; /\*\//!{N; bb}; s|/\*.*\*/|| }' -e 's|//.*||' "$f" |
+    grep -v '^#' |
+    grep -Eo "[\=\ \(\+\-\*\/\%\~\&\|\^\<\>]0([']?[0-7]+)+[Zz]?[UuLl]?[UuLl]?[UuLl]?[Zz]?" |
+    grep -Eo "$REGEXP"
+done
