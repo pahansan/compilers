e494a75f8430711954877c21823c1609028a2e52
Kulik Pavel <pashakulik58@gmail.com>
2025-05-12 22:23:45 +0700

    Поменял в графе unique_ptr на shared_ptr

--- 5lab/compilers-lab5-src/src/inheritance_graph.h
+++ 5lab/compilers-lab5-src/src/inheritance_graph.h
@@ -8,6 +8,7 @@
 #include <memory>
 #include <stack>
 #include <utility>
+#include <ranges>
 
 extern Program ast_root;
 
@@ -17,10 +18,12 @@ struct GraphNode
 {
     std::string class_name;
     Class_ class_;
-    std::vector<std::unique_ptr<GraphNode>> kids;
+    std::vector<std::shared_ptr<GraphNode>> kids;
 
     GraphNode(const std::string name = "", Class_ cl = nullptr) : class_name(name), class_(cl) {}
 };
+
+using graph_node_ptr = std::shared_ptr<GraphNode>;
 
 namespace std
 {
@@ -36,37 +39,65 @@ namespace std
 
 class Graph
 {
-    std::vector<std::unique_ptr<GraphNode>> first_level_;
+    std::vector<graph_node_ptr> first_level_;
 
 public:
     Graph()
     {
-        first_level_.push_back(std::make_unique<GraphNode>("Object"));
+        first_level_.push_back(std::make_shared<GraphNode>("Object"));
     }
 
-    GraphNode *find(const GraphNode &v)
+    graph_node_ptr find(const GraphNode &v)
     {
-        std::stack<GraphNode *> stack_;
+        std::stack<graph_node_ptr> stack_;
         for (auto &vertex : first_level_)
-            stack_.push(vertex.get());
+            stack_.push(vertex);
         std::vector<std::string> visited;
         while (!stack_.empty())
         {
-            GraphNode *vertex = stack_.top();
+            graph_node_ptr vertex = stack_.top();
             stack_.pop();
-            if (std::find(visited.begin(), visited.end(), vertex->class_name) == visited.end())
+            if (std::ranges::find(visited, vertex->class_name) == visited.end())
                 visited.push_back(vertex->class_name);
             else
                 continue;
 
-            if (vertex->class_name == v.class_name)
+            if ((*vertex).class_name == v.class_name)
                 return vertex;
             else
                 for (auto &kid : vertex->kids)
-                    stack_.push(kid.get());
+                    stack_.push(kid);
         }
         return nullptr;
     }
+
+    // void detach(const GraphNode &v)
+    // {
+    //     std::stack<GraphNode *> stack_;
+
+    //     for (auto &vertex : first_level_)
+    //     {
+    //         if (vertex->class_name == v.class_name)
+    //             first_level_.erase(std::ranges::find(first_level_, vertex));
+    //         stack_.push(vertex.get());
+    //     }
+    //     std::vector<std::string> visited;
+    //     while (!stack_.empty())
+    //     {
+    //         GraphNode *vertex = stack_.top();
+    //         stack_.pop();
+    //         if (std::find(visited.begin(), visited.end(), vertex->class_name) == visited.end())
+    //             visited.push_back(vertex->class_name);
+    //         else
+    //             continue;
+
+    //         if (vertex->class_name == v.class_name)
+    //             return vertex;
+    //         else
+    //             for (auto &kid : vertex->kids)
+    //                 stack_.push(kid.get());
+    //     }
+    // }
 
     bool add_edge(const GraphNode &parent, const GraphNode &kid)
     {
@@ -76,9 +107,9 @@ public:
         if (kid_vertex == nullptr)
         {
             if (parent_vertex == nullptr)
-                first_level_.push_back(std::make_unique<GraphNode>(kid.class_name, kid.class_));
+                first_level_.push_back(std::make_shared<GraphNode>(kid.class_name, kid.class_));
             else
-                parent_vertex->kids.push_back(std::make_unique<GraphNode>(kid.class_name, kid.class_));
+                parent_vertex->kids.push_back(std::make_shared<GraphNode>(kid.class_name, kid.class_));
         }
         else
         {
